7bf74ca5ec87b3832998f167c302d9e5889492b4
unknown <mrnafsu07@gmail.com>
2024-12-21 18:36:00 +0600

    added category file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Category></Category>
+      <Category2></Category2>
     </div>
   );
 };
